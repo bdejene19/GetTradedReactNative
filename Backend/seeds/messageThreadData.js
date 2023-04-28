3186def019fa0cfcb59267608fdae96357f6b753
Bemnet Dejene <bdejene19@gmail.com>
2023-04-28 18:12:54 -0400

    added basic seeding to begin backend routing, to call data to  frontend

---
+++ Backend/seeds/messageThreadData.js
@@ -0,0 +1,13 @@
+const messageThreadData = [
+  {
+    inbox_id: 1,
+  },
+  {
+    inbox_id: 2,
+  },
+  {
+    inbox_id: 3,
+  },
+];
+
+module.exports = messageThreadData;
--- Backend/seeds/seeds.js
+++ Backend/seeds/seeds.js
@@ -1,17 +1,21 @@
-const { User } = require("../Models/index");
+const { User, WorkLocation, Inbox, MessageThread } = require("../Models/index");
 const sequelize = require("../config/connection");
 const UserData = require("./userData");
-
+const locationData = require("./workLocations");
+const MessageData = require("./messageThreadData");
+const InboxData = require("./inboxData");
 const seedDatabase = async () => {
   await sequelize.sync({
     force: true,
   });
 
   await User.bulkCreate(UserData);
+  await WorkLocation.bulkCreate(locationData);
+  await Inbox.bulkCreate(InboxData);
+  await MessageThread.bulkCreate(MessageData);
   console.log("success seeding database");
 
   process.exit(0);
 };
 
 seedDatabase();
-  
